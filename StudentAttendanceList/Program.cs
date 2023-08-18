f98d798d8ca27c4fd6b3f66d5bf55a3cd8a30775
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-18 18:14:38 +0300

    Added list named `students`

--- StudentAttendanceList/Program.cs
+++ StudentAttendanceList/Program.cs
@@ -9,7 +9,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             //Student sınıfından nesnelerin listesini temsil eden student adında bir liste oluşturulur.
+            List<Student> students = new();
+            students.Add(new Student() { studentName = "Gaye", inClass = false });
+            students.Add(new Student() { studentName = "Cemre", inClass = true });
+            students.Add(new Student() { studentName = "Emir", inClass = false });
+            students.Add(new Student() { studentName = "Osman", inClass = false });
+            students.Add(new Student() { studentName = "Halid", inClass = true });
         }
     }
 }
