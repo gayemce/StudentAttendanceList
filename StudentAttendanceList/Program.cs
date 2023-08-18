7996298ad4c5ecf70b2d9bf81278551f6185a134
= <=>
2023-08-18 17:59:34 +0300

    Add project files.

---
+++ StudentAttendanceList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentAttendanceList
+{
+    public class Student
+    {
+        public string studentName;
+        public bool inClass;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
