d79ff6f0808c09eade1f68e859ebbf864852c780
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-18 18:21:01 +0300

    First action added

--- StudentAttendanceList/Program.cs
+++ StudentAttendanceList/Program.cs
@@ -23,6 +23,20 @@
             Console.WriteLine("2 - Yoklama Al");
             Console.WriteLine("3 - Gelmeyen Öğrenci Listesi");
             Console.WriteLine("4 - Çıkış\n");
+
+            while (true)
+            {
+                string result = Console.ReadLine();
+
+                if(result.ToLower() == "Öğrenci Listesi".ToLower() || result == "1")
+                {
+                    Console.WriteLine("* Öğrenci Listesi *\n");
+                    foreach (Student item in students)
+                    {
+                        Console.WriteLine($"{item.studentName} - {item.inClass}");
+                    }
+                }
+            }    
         }
     }
 }
