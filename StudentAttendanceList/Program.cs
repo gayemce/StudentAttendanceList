50d5d8af1264002803d58873779ddac43e8fe958
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-19 17:31:46 +0300

    second action updated

--- StudentAttendanceList/Program.cs
+++ StudentAttendanceList/Program.cs
@@ -44,6 +44,36 @@
                     {
                         Console.WriteLine($"{i + 1}. {students[i].studentName}");
                     }
+
+                    tekrar1:;
+                    Console.WriteLine("Yoklama alacağınız öğrencinin numarasını yazın:");
+                    string selectedStudentString = Console.ReadLine();
+                    int selectedStudent = 0;
+                    if (!int.TryParse(selectedStudentString, out selectedStudent))
+                    {
+                        Console.WriteLine("* Uyarı: Lütfen sayı değeri giriniz:");
+                        goto tekrar1;
+                    }
+                    
+                    if(selectedStudent > students.Count)
+                    {
+                        Console.WriteLine("* Uyarı: Böyle bir öğrenci bulunmamaktadır.");
+                        goto tekrar1;
+                    }
+
+
+                    Console.WriteLine($"Seçilen öğrenci: {students[selectedStudent - 1].studentName} \nÖğrenci sınıfta mı? \nTrue or False");
+                    string teacherResult = Console.ReadLine();
+                    if(teacherResult.ToLower() == "True".ToLower())
+                    {
+                        Console.WriteLine();
+                        Student student = new();
+                        students[selectedStudent - 1].inClass = true;
+                        foreach (Student item in students)
+                        {
+                            Console.WriteLine($"{item.studentName} - {item.inClass}");
+                        }
+                    }
                 }
             }    
         }
